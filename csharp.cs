76ae2b761b910fde8e27fd80ba0bcc9d0f8675fc
yasmeen <yasmien960@gmail.com>
2024-01-21 13:57:54 +0200

    csharp.cs

---
+++ csharp.cs
@@ -0,0 +1,17 @@
+using System; 
+  
+
+namespace HelloWorldApp { 
+     
+    class Geeks { 
+          
+   
+        static void Main(string[] args) { 
+              
+          
+            Console.WriteLine("Hello World!"); 
+           
+            Console.ReadKey(); 
+        } 
+    } 
+}
